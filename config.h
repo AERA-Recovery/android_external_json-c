79a8ff67c9ec24770b0aacf22ed779a94b1937c9
Jonas Salo <salomannen1234@gmail.com>
2026-05-02 03:43:59 +0200

    fix build errors

---
+++ config.h
@@ -0,0 +1,47 @@
+#ifndef JSON_C_CONFIG_H
+#define JSON_C_CONFIG_H
+
+#define STDC_HEADERS 1
+
+#define HAVE_DLFCN_H 1
+#define HAVE_ERRNO_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_LOCALE_H 1
+#define HAVE_MATH_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDARG_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYSLOG_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+
+#define HAVE_DECL_INFINITY 1
+#define HAVE_DECL_NAN 1
+
+#define HAVE___THREAD 1
+#define SPEC___THREAD __thread
+
+#define HAVE_OPEN 1
+#define HAVE_REALLOC 1
+#define HAVE_SETLOCALE 1
+#define HAVE_SNPRINTF 1
+#define HAVE_STRCASECMP 1
+#define HAVE_STRDUP 1
+#define HAVE_STRERROR 1
+#define HAVE_STRNCASECMP 1
+#define HAVE_USELOCALE 1
+#define HAVE_USLEEP 1
+#define HAVE_VASPRINTF 1
+#define HAVE_VSNPRINTF 1
+
+#define HAVE_ISINF 1
+#define HAVE_ISNAN 1
+#define HAVE_FINITE 1
+
+#endif
---
+++ json.h
@@ -0,0 +1,19 @@
+#ifndef JSON_C_JSON_H
+#define JSON_C_JSON_H
+
+#include "json_config.h"
+#include "json_c_version.h"
+#include "json_types.h"
+#include "arraylist.h"
+#include "debug.h"
+#include "linkhash.h"
+#include "printbuf.h"
+#include "json_object.h"
+#include "json_object_iterator.h"
+#include "json_tokener.h"
+#include "json_util.h"
+#include "json_visit.h"
+#include "json_pointer.h"
+#include "json_patch.h"
+
+#endif
